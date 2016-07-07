cbff373500075c83b3244e2899d19bdc55f6dd30
hnamitha1 <namitha.h@gmail.com>
2016-07-07 20:59:57 +0100

    nice styling

--- spec/features/editing_posts_spec.rb
+++ spec/features/editing_posts_spec.rb
@@ -1,26 +1,47 @@
 require 'rails_helper'
 
-feature 'Editing posts' do  
+feature 'editing posts' do  
   background do
-    job = create(:post)
     user = create :user
-    sign_in_with user 
-   
-    find(:xpath, "//a[contains(@href,'posts/1')]").click
-    click_link 'Edit Post'
+    user_two = create(:user, email: 'hi@hi.com',
+                             user_name: 'BennyBoy',
+                             id: user.id + 1)
+    post = create(:post, user_id: user.id)
+    post_two = create(:post, user_id: user.id + 1)
+
+    sign_in_with user
+    visit '/'
   end
-  scenario 'Can edit a post' do
+
+  scenario 'can edit a post as the owner' do
+    find(:xpath, "//a[contains(@href,'posts/1')]").click
+    expect(page).to have_content('Edit Post')
+
+    click_link 'Edit Post'
     fill_in 'Caption', with: "Oh god, you weren't meant to see this picture!"
     click_button 'Update Post'
 
-    expect(page).to have_content("Post updated.")
+    expect(page).to have_content('Post updated.')
     expect(page).to have_content("Oh god, you weren't meant to see this picture!")
   end
 
-  it "won't update a post without an image" do  
-	  attach_file('Image', 'spec/files/coffee.zip')
-	  click_button 'Update Post'
+  scenario "cannot edit a post that doesn't belong to you via the show page" do
+    find(:xpath, "//a[contains(@href,'posts/2')]").click
+    expect(page).to_not have_content('Edit Post')
+  end
 
-	  expect(page).to have_content("Update failed.  Please check the form.")
-	end 
-end 
+  scenario "cannot edit a post that doesn't belong to you via url path" do
+    visit "/posts/2/edit"
+    expect(page.current_path).to eq root_path
+    expect(page).to have_content("That post doesn't belong to you!")
+  end
+
+  scenario "a post won't update without an attached image" do
+    find(:xpath, "//a[contains(@href,'posts/1')]").click
+    click_link 'Edit Post'
+    attach_file('Image', 'spec/files/coffee.zip')
+    click_button 'Update Post'
+
+    expect(page).to have_content('Update failed.  Please check the form.')
+  end
+end  
