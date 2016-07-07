e73061e4666b0c7a0af0975da94c282cb4ce7f8e
hnamitha1 <namitha.h@gmail.com>
2016-07-07 12:45:47 +0100

    username added

--- spec/features/creating_posts_spec.rb
+++ spec/features/creating_posts_spec.rb
@@ -12,6 +12,7 @@ feature 'Creating posts' do
     fill_in 'Caption', with: 'nom nom nom #coffeetime' 
     click_button 'Create Post'
     expect(page).to have_content('#coffeetime')
+    expect(page).to have_content('Arnie')
     expect(page).to have_css("img[src*='coffee.jpg']")
   end
 
